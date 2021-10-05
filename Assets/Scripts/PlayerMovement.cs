e9e909b73d6a0e156d30d2b8ff156218209abd97
mazen <m.shaar@saedubai.com>
2021-10-05 14:07:20 +0400

    Jump + gravity customization
    Might need to configure them for later use depending on how the game needs to be played

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float movementSpeed = 5f;
+    public float jumpHeight = 5f;
+    public float gravity = -20f;
+
+    public Transform groundCheck;
+    public LayerMask groundTag;
+    public float groundHeight = 1f;
+    private bool isGrounded = false;
+    Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +26,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundHeight, groundTag);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * movementSpeed * Time.deltaTime);
+        controller.Move(move * movementSpeed *Time.deltaTime);
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
