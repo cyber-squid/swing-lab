44c6abb95a62140dd2d0e8729dc2dc6a46eb67db
mazen <m.shaar@saedubai.com>
2021-11-21 12:19:34 +0400

    Update 1.3.9
    - resized AI so its more of an assistance
    - fixed it so it works this time.
    - made it teleport, still need to work on some things for it

--- Assets/Scripts/Player Scripts/Player Character/PlayerCheckPoint.cs
+++ Assets/Scripts/Player Scripts/Player Character/PlayerCheckPoint.cs
@@ -6,8 +6,8 @@ public class PlayerCheckPoint : MonoBehaviour
 {
     public GrapplingHookL GrappleL;
     public GrapplingHookR GrappleR;
-    // private AIFollow aIFollow;
-    // public Transform levelposition;
+    private AIFollow aIFollow;
+    public Transform levelposition;
 
     //public GameObject CheckPointMark;
     Vector3 spawnPoint;
@@ -17,7 +17,7 @@ public class PlayerCheckPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //aIFollow = FindObjectOfType<AIFollow>();
+        aIFollow = FindObjectOfType<AIFollow>();
         spawnPoint = gameObject.transform.position;
         GrappleL = GetComponentInChildren<GrapplingHookL>();
         GrappleR = GetComponentInChildren<GrapplingHookR>();
@@ -38,9 +38,9 @@ public class PlayerCheckPoint : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Checkpoint"))
         {
-            //  aIFollow.follow = false;
-            //  aIFollow.goTolevelposition = true;
-            //  aIFollow.targetposition = levelposition.position;
+            aIFollow.follow = false;
+            aIFollow.goTolevelposition = true;
+            aIFollow.targetposition = levelposition.position;
             spawnPoint = other.gameObject.transform.position;
             CheckpointSound.Play();
             Destroy(other.gameObject);
