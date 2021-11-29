e9831d0b8f408b3642de1fa1b117f29f67f5f08f
SAESD <s.dummar@student.saedubai.com>
2021-11-29 04:47:56 -0800

    Update 1.4.7
    - Added Toggles For Post Processing Effects And Background Music
    - Improved Main Menu Scene
    - Fixed another issue regarding the text UI in the tutorial scene (adjusted colliders)
    - Fixed and optimized footsteps sound effect

--- Assets/_Scripts/Menu Scripts/MenuPause.cs
+++ Assets/_Scripts/Menu Scripts/MenuPause.cs
@@ -7,6 +7,16 @@ public class MenuPause : MonoBehaviour
 {
     public GameObject pauseMenu;
     private bool isPaused;
+
+    public GameObject MotionBlurObject;
+    public GameObject PaniniProjectionObject;
+    public GameObject VignetteObject;
+    public GameObject LensDistortionObject;
+    public GameObject FilmGrainObject;
+
+    public GameObject BackgroundMusicObject;
+
+
     void Start()
     {
         pauseMenu.SetActive(false);
@@ -48,5 +58,78 @@ public class MenuPause : MonoBehaviour
     {
         SceneManager.LoadScene("Main Menu");
         Time.timeScale = 1f;
+    }
+
+    public void MotionBlurToggle()
+    {
+        if (MotionBlurObject.activeInHierarchy == true)
+        {
+            MotionBlurObject.SetActive(false);
+        }
+        else
+        {
+            MotionBlurObject.SetActive(true);
+        }
+    }
+
+    public void PaniniProjectionToggle()
+    {
+        if (PaniniProjectionObject.activeInHierarchy == true)
+        {
+            PaniniProjectionObject.SetActive(false);
+        }
+        else
+        {
+            PaniniProjectionObject.SetActive(true);
+        }
+    }
+
+    public void VignetteToggle()
+    {
+        if (VignetteObject.activeInHierarchy == true)
+        {
+            VignetteObject.SetActive(false);
+        }
+        else
+        {
+            VignetteObject.SetActive(true);
+        }
+    }
+
+    public void LensDistortionToggle()
+    {
+        if (LensDistortionObject.activeInHierarchy == true)
+        {
+            LensDistortionObject.SetActive(false);
+        }
+        else
+        {
+            LensDistortionObject.SetActive(true);
+        }
+    }
+
+    public void FilmGrainToggle()
+    {
+        if (FilmGrainObject.activeInHierarchy == true)
+        {
+            FilmGrainObject.SetActive(false);
+        }
+        else
+        {
+            FilmGrainObject.SetActive(true);
+        }
+
+    }
+
+    public void BackgroundMusicToggle()
+    {
+        if (BackgroundMusicObject.activeInHierarchy == true)
+        {
+            BackgroundMusicObject.SetActive(false);
+        }
+        else
+        {
+            BackgroundMusicObject.SetActive(true);
+        }
     }
 }
