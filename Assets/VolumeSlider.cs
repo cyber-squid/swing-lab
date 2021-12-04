2e98cf44c7eeaceffb2b57a17f2542bdc3802db3
mazen <m.shaar@saedubai.com>
2021-12-04 14:34:02 +0400

    Update 1.4.9
    - created a test animation for AI
    - Added a Volume Slider

---
+++ Assets/VolumeSlider.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    Slider slider;
+    void Awake() {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        AudioListener.volume = slider.value;
+    }
+}
